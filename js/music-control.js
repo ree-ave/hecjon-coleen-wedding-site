5fd7b57fd640dc0e4001b77e0f9869af6907a020
ree-ave <rizelle.estolas@gmail.com>
2025-12-13 21:53:00 +0800

    Fix JavaScript syntax error in timeline click handler

--- js/music-control.js
+++ js/music-control.js
@@ -181,11 +181,11 @@
     timelineImg.style.cursor = 'pointer';
     timelineImg.addEventListener('click', function() {
       const modal = document.createElement('div');
-      modal.style.cssText = 
+      modal.style.cssText = `
         position: fixed; top: 0; left: 0; width: 100%; height: 100%;
         background-color: rgba(0, 0, 0, 0.8); display: flex;
         align-items: center; justify-content: center; z-index: 10002;
-      ;
+      `;
       
       const enlargedImg = document.createElement('img');
       enlargedImg.src = this.src;
